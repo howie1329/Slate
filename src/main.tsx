9ce423eb4dd039690b85176e3b4e9272bdac1aa0
howie1329 <46204910+howie1329@users.noreply.github.com>
2026-07-20 20:54:47 -0400

    chore: initialize shadcn foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { router } from "./router";
 import "./styles.css";
 
@@ -12,6 +14,9 @@ declare module "@tanstack/react-router" {
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <TooltipProvider>
+      <RouterProvider router={router} />
+      <Toaster />
+    </TooltipProvider>
   </React.StrictMode>,
 );
